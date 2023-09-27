36448f5d60841c2abad13190142cbc066fdea65b
Fathan Abi <abikarami60@gmail.com>
2023-09-27 09:54:51 +0700

    Tugas 4

---
+++ index.js
@@ -0,0 +1,5 @@
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let result = input1 + input2;
+
+document.getElementById("result").innerHTML = "<p>" + result + "</p>";
