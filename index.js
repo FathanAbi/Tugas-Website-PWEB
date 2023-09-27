a0292aeb53b528a73b9237110012d1bc9874806c
Fathan Abi <abikarami60@gmail.com>
2023-09-27 10:18:14 +0700

    Tugas4

--- index.js
+++
@@ -1,5 +0,0 @@
-let input1 = document.getElementById("input1");
-let input2 = document.getElementById("input2");
-let result = input1 + input2;
-
-document.getElementById("result").innerHTML = "<p>" + result + "</p>";
