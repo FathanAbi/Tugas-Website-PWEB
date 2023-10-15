24aaf4be3deda8ca3125a15ef2c1f2ab827d6abb
Fathan Abi <abikarami60@gmail.com>
2023-10-15 19:55:27 +0700

    Work weather Api

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-function getList(){
+function getWeatherInfo() {
     const data = null;
 
+    let city = document.getElementById("city").value;
+    console.log(city);
+
     const xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
+    xhr.withCredentials = true;
 
     xhr.addEventListener('readystatechange', function () {
         if (this.readyState === this.DONE) {
-            let result = document.getElementById("video-list");
-            const JSONResult = JSON.parse(this.responseText)
-            let items = JSONResult.items;
-            items.forEach(video => {
-                // console.log(video.snippet.title);
-                // result.innerHTML = video.snippet.title;
-
-                const listItem = document.createElement('li'); // Create a new list item
-
-                listItem.textContent = video.snippet.title; // Set the text content of the list item
-
-                result.appendChild(listItem); // Append the list item to the results list
-                
-            });
-            
-            
-            
+            console.log(this.responseText);
+            let weather = JSON.parse(this.responseText)
+            const weatherInfo = document.getElementById('weather-info');
+                    weatherInfo.innerHTML = `
+                        <h2>Weather in ${weather.location.name}</h2>
+                        <p>Temperature: ${weather.current.temp_c}°C</p>
+                        <p>Weather: ${weather.current.condition.text}</p>
+                        
+                    `;
         }
     });
 
-    let search = document.getElementById("search");
-    console.log(search.value);
-    let url = "https://youtube-v31.p.rapidapi.com/search?q=";
-    url = url+search.value;
-    url = url + "&part=snippet%2Cid&regionCode=US&maxResults=50&order=date";
-    console.log(url);
-
+    let url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`;
 
     xhr.open('GET', url);
     xhr.setRequestHeader('X-RapidAPI-Key', '011135629dmsh1a9714889029b7ep12da5djsnc9cb36f3ae86');
-    xhr.setRequestHeader('X-RapidAPI-Host', 'youtube-v31.p.rapidapi.com');
+    xhr.setRequestHeader('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com');
 
     xhr.send(data);
-
 }
